ef853b362d4bf72106344a8bb2b8da113e080484
funnyfrog777 <116278556+funnyfrog777@users.noreply.github.com>
2022-11-22 23:44:34 -0500

    update

--- src/App.js
+++ src/App.js
@@ -12,26 +12,32 @@ recipeData.recipes.forEach((item) => {
 recipeData.ingredients.sort();
 
 function App() {
+  const [ingredients, setIngredients] = useState(recipeData.ingredients);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Breath of the Wild Recipes</h1>
       </header>
       <body className="App-body">
-        <br/>
+        <div>
+          <p>{ingredients.length}</p>
+        </div>
         <div className="Filter-display">
           <fieldset>
             <legend>Ingredients</legend>
-            <div><input type="checkbox" id="All Ingredients" name="All Ingredients"/><label for="All Ingredients" className="small-font"> All Ingredients </label></div>
+            <div><input type="checkbox" id="All Ingredients" name="All Ingredients" checked/><label for="All Ingredients" className="small-font"> All Ingredients </label></div>
             {recipeData.ingredients.map((item, index) => (
-              <Filter name={item}/>
+              <Filter name={item} ingredients={ingredients} setIngredients={setIngredients}/>
             ))}
           </fieldset>
         </div>
         <div className="Recipe-display">
-          {recipeData['recipes'].map((item, index) => (
-            <MealItem img={item.image} name={item.name} desc={item.desc} resale={item.resale} ingredients={item.ingredients}/>
-          ))}
+          {recipeData.recipes.map((item, index) => {
+            if (item.ingredients.every(i=> ingredients.includes(i))) {
+              return <MealItem img={item.image} name={item.name} desc={item.desc} resale={item.resale} ingredients={item.ingredients}/>
+            }
+          })}
         </div>
         <br/>
       </body>
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,19 @@
-export default function Filter({name}) {
+export default function Filter({name, ingredients, setIngredients}) {
+
+    const handleClick = () => {
+        var array = [...ingredients];
+        var index = array.indexOf(name);
+        if (index !== -1) {
+            array.splice(index, 1);
+            setIngredients(array);
+        } else {
+            setIngredients([...array, name]);
+        }
+    }
+
     return (
         <div className="inline">
-            <input type="checkbox" id={name} name={name}/>
+            <input type="checkbox" id={name} name={name} onClick={handleClick} checked/>
             <label for={name} className="small-font"> {name} </label>
         </div>
     )
