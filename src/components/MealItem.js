7851ef762d6c8d89fb986e998173d7815bf83795
funnyfrog777 <helen_huang+cs1300@brown.edu>
2022-12-27 14:08:06 -0500

    made cards smaller

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -5,13 +5,13 @@ export default function MealItem({img, name, desc, resale, ingredients}) {
                 <p className="center">{name}</p>
                 <img className="image" src={img} alt={name}/>
                 <span><img className="icon-2" src="images/Rupee.png" alt={"Rupee"}/> {resale}</span>
-                <span className="description"><i>{desc}</i></span>
+                <span className="description center"><i>{desc}</i></span>
             </div>
-            <div className="dark-green">
+            <div className="Meal-item-ingredients">
                 <span className="label">Ingredients: </span>
-                <div className="description">
+                <div className="description small-font">
                     {ingredients.map((item, index) => (
-                        <p>- {item}</p>
+                        <p className="small-margin">- {item}</p>
                     ))}
                 </div>
             </div>
